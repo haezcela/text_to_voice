4d261dc84c33facadb113accc1360db78ab0f5f7
haezcela <aleczhysa@gmail.com>
2024-01-15 15:56:27 +0800

    Added mic functions

--- assets/js/reply.js
+++ assets/js/reply.js
@@ -1,11 +1,46 @@
 const artyom = new Artyom();
-document.getElementById("btn").addEventListener(
-  "click",
-  function () {
+
+var micButton = document.getElementById("btn");
+var micIcon = document.getElementById("micIcon");
+
+// Set initial state
+var isRecording = false;
+
+micButton.addEventListener("click", function () {
+  // Toggle recording state
+  isRecording = !isRecording;
+
+  // Change button text and icon based on the recording state
+  if (isRecording) {
+    micButton.innerHTML = "OFF MIC";
+    // Change the button icon to the recording GIF (replace 'recording.gif' with your actual file path)
+    micIcon.src = "assets/icons/microphone-active.gif"; // Replace with the path to the image when the microphone is on
     artyom.say("Please speak");
-  },
-  false
-);
+    artyom
+      .initialize({
+        lang: "en-GB",
+        continuous: true,
+        soundex: true,
+        debug: true,
+        executionKeyword: "and do it now",
+        listen: true,
+      })
+      .then(() => {
+        console.log("Artyom has been successfully initialized");
+        $("console").append("Please speak to execute command");
+      })
+      .catch((err) => {
+        console.log("Artyom coudn't be initialized", err);
+        $("console").append("Artyom coudn't be initialized", err);
+      });
+  } else {
+    micButton.innerHTML = "Open Mic";
+    // Change the button icon back to the initial state (replace 'initial.gif' with your actual file path)
+    micIcon.src = "assets/icons/no-microphone.gif"; // Replace with the path to the initial image
+    artyom.say("microphone off");
+    artyom.fatality();
+  }
+});
 
 artyom.addCommands([
   {
@@ -30,20 +65,7 @@ artyom.addCommands([
   },
 ]);
 
-artyom
-  .initialize({
-    lang: "en-GB",
-    continuous: true,
-    soundex: true,
-    debug: true,
-    executionKeyword: "and do it now",
-    listen: true,
-  })
-  .then(() => {
-    console.log("Artyom has been successfully initialized");
-    $("console").append("Please speak to execute command");
-  })
-  .catch((err) => {
-    console.log("Artyom coudn't be initialized", err);
-    $("console").append("Artyom coudn't be initialized", err);
-  });
+artyom.when("NOT_COMMAND_MATCHED", function () {
+  // Handle the case when no command is matched
+  artyom.say("I'm sorry, I don't have an answer for that.");
+});
